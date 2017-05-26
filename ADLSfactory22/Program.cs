8372ddb9a7b0d177c48921d7bada3d227459c01e
sid-choudhary <siddhant07324@gmail.com>
2017-05-26 09:28:01 +0530

    Add project files.

---
+++ ADLSfactory22/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure;
+using Microsoft.Azure.Management.DataFactories;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories.Common.Models;
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Clients.ActiveDirectory.Internal;
+
+namespace ADLSfactory22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create data factory management client
+            string resourceGroupName = "t-sichoudemo";
+            string dataFactoryName = "ADLSdemoooo3";
+
+            TokenCloudCredentials aadTokenCredentials = new TokenCloudCredentials(
+                    ConfigurationManager.AppSettings["SubscriptionId"],
+                    GetAuthorizationHeader().Result);
+
+            Uri resourceManagerUri = new Uri(ConfigurationManager.AppSettings["ResourceManagerEndpoint"]);
+
+            DataFactoryManagementClient client = new DataFactoryManagementClient(aadTokenCredentials, resourceManagerUri);
+
+            // create a data factory
+            Console.WriteLine("Creating a data factory");
+            client.DataFactories.CreateOrUpdate(resourceGroupName,
+                new DataFactoryCreateOrUpdateParameters()
+                {
+                    DataFactory = new DataFactory()
+                    {
+                        Name = dataFactoryName,
+                        Location = "westus",
+                        Properties = new DataFactoryProperties()
+                    }
+                }
+            );
+
+            // create a linked service for input data store: Azure Storage
+            Console.WriteLine("Creating Azure Storage linked service");
+            client.LinkedServices.CreateOrUpdate(resourceGroupName, dataFactoryName,
+                new LinkedServiceCreateOrUpdateParameters()
+                {
+                    LinkedService = new LinkedService()
+                    {
+                        Name = "AzureStorageLinkedService",
+                        Properties = new LinkedServiceProperties
+                        (
+                            new AzureStorageLinkedService("DefaultEndpointsProtocol=https;AccountName=tsichouas;AccountKey=ZKZBY4UH7un/4up/KH6drc76ta92tEaXZ9/N1obX2obGGwxd9sYQz8iKywFff4cqrQNFl4ImLfZDTIg5xrhOBA==;EndpointSuffix=core.windows.net")
+                        )
+                    }
+                }
+            );
+
+
+            // create a linked service for output data store: ADLS
+
+            Console.WriteLine("Creating ADLS linked service");
+            client.LinkedServices.CreateOrUpdate(resourceGroupName, dataFactoryName,
+                new LinkedServiceCreateOrUpdateParameters()
+                {
+                    LinkedService = new LinkedService()
+                    {
+                        Name = "AzureDataLakeStoreLinkedService",
+                        Properties = new LinkedServiceProperties
+                        (
+                            new AzureDataLakeStoreLinkedService()
+                            {
+                                DataLakeStoreUri= "adl://tsichouadls.azuredatalakestore.net",
+                                SubscriptionId = "796eaf7c-2e26-4938-9b94-0f2369c33d74",
+                                ResourceGroupName= "t-sichoudemo",
+                                ServicePrincipalId= "b32b83c2-3ca2-4580-a967-0fceb8f29295",
+                                ServicePrincipalKey= "dD+Dfk6fgQEIJksSOdKXYas/00UTC4EPHP85ZiofVKw=",
+                                Tenant= "72f988bf-86f1-41af-91ab-2d7cd011db47"
+                            }
+                            
+                        )
+                    }
+                }
+            );
+
+            // create input and output datasets
+            Console.WriteLine("Creating input and output datasets");
+            string Dataset_Source = "InputDataset";
+            string Dataset_Destination = "OutputDataset";
+
+            Console.WriteLine("Creating input dataset of type: Azure Blob");
+            client.Datasets.CreateOrUpdate(resourceGroupName, dataFactoryName,
+
+            new DatasetCreateOrUpdateParameters()
+            {
+                Dataset = new Dataset()
+                {
+                    Name = Dataset_Source,
+                    Properties = new DatasetProperties()
+                    {
+                        Structure = new List<DataElement>()
+                        {
+                new DataElement() { Name = "FirstName", Type = "String" },
+                new DataElement() { Name = "LastName", Type = "String" }
+                        },
+                        LinkedServiceName = "AzureStorageLinkedService",
+                        TypeProperties = new AzureBlobDataset()
+                        {
+                            FolderPath = "t-sichoublob/",
+                            FileName = "empl.txt"
+                        },
+                        External = true,
+                        Availability = new Availability()
+                        {
+                            Frequency = SchedulePeriod.Hour,
+                            Interval = 1,
+                        },
+
+                        Policy = new Policy()
+                        {
+                            Validation = new ValidationPolicy()
+                            {
+                                MinimumRows = 1
+                            }
+                        }
+                    }
+                }
+            });
+
+            Console.WriteLine("Creating output dataset of type: ADLS");
+            client.Datasets.CreateOrUpdate(resourceGroupName, dataFactoryName,
+                new DatasetCreateOrUpdateParameters()
+                {
+                    Dataset = new Dataset()
+                    {
+                        Name = Dataset_Destination,
+                        Properties = new DatasetProperties()
+                        {
+
+                            LinkedServiceName = "AzureDataLakeStoreLinkedService",
+                            TypeProperties = new AzureDataLakeStoreDataset()
+                            {
+                                FileName = "tsichuodhary.tsv",
+                                FolderPath = "demo/",
+                            },
+                            Availability = new Availability()
+                            {
+                                Frequency = SchedulePeriod.Hour,
+                                Interval = 1,
+                            },
+                        }
+
+                    }
+                });
+
+
+            // create a pipeline
+            Console.WriteLine("Creating a pipeline");
+            DateTime PipelineActivePeriodStartTime = new DateTime(2017, 5, 22, 0, 0, 0, 0, DateTimeKind.Utc);
+            DateTime PipelineActivePeriodEndTime = new DateTime(2017, 5, 23, 0, 0, 0, 0, DateTimeKind.Utc);
+            string PipelineName = "ADFTutorialPipeline";
+
+            client.Pipelines.CreateOrUpdate(resourceGroupName, dataFactoryName,
+                new PipelineCreateOrUpdateParameters()
+                {
+                    Pipeline = new Pipeline()
+                    {
+                        Name = PipelineName,
+                        Properties = new PipelineProperties()
+                        {
+                            Description = "Demo Pipeline for data transfer between blobs",
+
+                            // Initial value for pipeline's active period. With this, you won't need to set slice status
+                            Start = PipelineActivePeriodStartTime,
+                            End = PipelineActivePeriodEndTime,
+
+                            Activities = new List<Activity>()
+                            {
+                    new Activity()
+                    {
+                        Name = "BlobToAzureSql",
+                        Inputs = new List<ActivityInput>()
+                        {
+                            new ActivityInput() {
+                                Name = Dataset_Source
+                            }
+                        },
+                        Outputs = new List<ActivityOutput>()
+                        {
+                            new ActivityOutput()
+                            {
+                                Name = Dataset_Destination
+                            }
+                        },
+                        TypeProperties = new CopyActivity()
+                        {
+                            Source = new BlobSource(),
+                            Sink = new AzureDataLakeStoreSink()
+                            {
+                                WriteBatchSize = 10000,
+                                WriteBatchTimeout = TimeSpan.FromMinutes(10)
+                            }
+                        },
+                        Scheduler=   new Scheduler()
+                        {
+                            Frequency=SchedulePeriod.Hour,
+                            Interval=1,
+                        }                    
+                    }
+                            }
+                        }
+                    }
+                });
+
+            // Pulling status within a timeout threshold
+            DateTime start = DateTime.Now;
+            bool done = false;
+
+            while (DateTime.Now - start < TimeSpan.FromMinutes(5) && !done)
+            {
+                Console.WriteLine("Pulling the slice status");
+                // wait before the next status check
+                Thread.Sleep(1000 * 12);
+
+                var datalistResponse = client.DataSlices.List(resourceGroupName, dataFactoryName, Dataset_Destination,
+                    new DataSliceListParameters()
+                    {
+                        DataSliceRangeStartTime = PipelineActivePeriodStartTime.ConvertToISO8601DateTimeString(),
+                        DataSliceRangeEndTime = PipelineActivePeriodEndTime.ConvertToISO8601DateTimeString()
+                    });
+
+                foreach (DataSlice slice in datalistResponse.DataSlices)
+                {
+                    if (slice.State == DataSliceState.Failed || slice.State == DataSliceState.Ready)
+                    {
+                        Console.WriteLine("Slice execution is done with status: {0}", slice.State);
+                        done = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Slice status is: {0}", slice.State);
+                    }
+                }
+            }
+
+            Console.WriteLine("Getting run details of a data slice");
+
+            // give it a few minutes for the output slice to be ready
+            Console.WriteLine("\nGive it a few minutes for the output slice to be ready and press any key.");
+            Console.ReadKey();
+
+            var datasliceRunListResponse = client.DataSliceRuns.List(
+                    resourceGroupName,
+                    dataFactoryName,
+                    Dataset_Destination,
+                    new DataSliceRunListParameters()
+                    {
+                        DataSliceStartTime = PipelineActivePeriodStartTime.ConvertToISO8601DateTimeString()
+                    }
+                );
+
+            foreach (DataSliceRun run in datasliceRunListResponse.DataSliceRuns)
+            {
+                Console.WriteLine("Status: \t\t{0}", run.Status);
+                Console.WriteLine("DataSliceStart: \t{0}", run.DataSliceStart);
+                Console.WriteLine("DataSliceEnd: \t\t{0}", run.DataSliceEnd);
+                Console.WriteLine("ActivityId: \t\t{0}", run.ActivityName);
+                Console.WriteLine("ProcessingStartTime: \t{0}", run.ProcessingStartTime);
+                Console.WriteLine("ProcessingEndTime: \t{0}", run.ProcessingEndTime);
+                Console.WriteLine("ErrorMessage: \t{0}", run.ErrorMessage);
+            }
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+
+        }
+
+
+
+        public static async Task<string> GetAuthorizationHeader()
+        {
+            AuthenticationContext context = new AuthenticationContext(ConfigurationManager.AppSettings["ActiveDirectoryEndpoint"] + ConfigurationManager.AppSettings["ActiveDirectoryTenantId"]);
+            ClientCredential credential = new ClientCredential(
+                ConfigurationManager.AppSettings["ApplicationId"],
+                ConfigurationManager.AppSettings["Password"]);
+            AuthenticationResult result = await context.AcquireTokenAsync(
+                resource: ConfigurationManager.AppSettings["WindowsManagementUri"],
+                clientCredential: credential);
+
+            if (result != null)
+                return result.AccessToken;
+
+            throw new InvalidOperationException("Failed to acquire token");
+        }
+    }
+}
